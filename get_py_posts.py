bb0d1f2f341231a45950064ebc86516716de5f73
voron434 <070809010@mail.ru>
2017-03-13 00:11:33 +0300

    working with names

--- get_py_posts.py
+++ get_py_posts.py
@@ -1,17 +1,17 @@
 import json
 import requests
 
-def keyword_search():
+def get_python_posts():
     url = 'https://api.vk.com/method/newsfeed.search'
     params = {
             'q': 'python программирование -кожа -помогите -помощь',
             'count': 200,
             }
-    all_news = json.loads(requests.get(url, params).text)
-    return all_news
+    py_posts = json.loads(requests.get(url, params).text)
+    return py_posts
 
 if __name__ == "__main__":
-    py_posts = keyword_search()
+    py_posts = get_python_posts()
     path = 'vk_py_groups.json'
     with open(path, mode="w", encoding='utf-8') as my_file:
         json.dump(py_posts, my_file)
