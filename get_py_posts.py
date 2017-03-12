4703bf8a61f0ba39ec5d21692cb63b8cef78b4fe
voron434 <070809010@mail.ru>
2017-03-13 00:28:58 +0300

    smth was broke...

--- get_py_posts.py
+++ get_py_posts.py
@@ -1,17 +1,17 @@
 import json
 import requests
 
-def get_python_posts():
+def get_py_posts():
     url = 'https://api.vk.com/method/newsfeed.search'
     params = {
-            'q': 'python программирование -кожа -помогите -помощь',
-            'count': 200,
-            }
+        'q': 'python программирование -кожа -помогите -помощь',
+        'count': 200,
+        }
     py_posts = json.loads(requests.get(url, params).text)
     return py_posts
 
 if __name__ == "__main__":
-    py_posts = get_python_posts()
+    py_posts = get_py_posts()
     path = 'vk_py_groups.json'
     with open(path, mode="w", encoding='utf-8') as my_file:
         json.dump(py_posts, my_file)
