8286b6e683386e5040d3a7ad424d3d9022881ed1
voron434 <070809010@mail.ru>
2017-03-13 00:00:47 +0300

    init commit

---
+++ get_py_posts.py
@@ -0,0 +1,19 @@
+import json
+import requests
+
+#TODO: sort news for no repeat in task 2!!!
+
+def keyword_search():
+    url = 'https://api.vk.com/method/newsfeed.search'
+    params = {
+            'q': 'python программирование -кожа -помогите -помощь',
+            'count': 200,
+            }
+    all_news = json.loads(requests.get(url, params).text)
+    return all_news
+
+if __name__ == "__main__":
+    py_posts = keyword_search()
+    path = 'vk_py_groups.json'
+    with open(path, mode="w", encoding='utf-8') as my_file:
+        json.dump(py_posts, my_file)
