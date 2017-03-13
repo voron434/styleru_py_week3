651e7210d597f2d1331ad41c4eb0919c319c1394
voron434 <070809010@mail.ru>
2017-03-13 10:38:30 +0300

    refactoring previous

--- get_py_posts.py
+++ get_py_posts.py
@@ -2,58 +2,42 @@ import json
 import requests
 import os
 
-def get_new_py_posts():
+
+def fetch_python_posts_from_vk():
     url = 'https://api.vk.com/method/newsfeed.search'
     params = {
         'q': 'python программирование -кожа -помогите -помощь',
         'count': 200,
         }
-    new_py_posts = json.loads(requests.get(url, params).text)
-    new_py_posts['response'].pop(0)
-    return new_py_posts['response']
+    new_python_posts = json.loads(requests.get(url, params).text)
+    new_python_posts['response'].pop(0)
+    return new_python_posts['response']
 
-def chek_post(py_post, old_py_posts):
-    for old_py_post in old_py_posts:
-        if py_post['text'] == old_py_post['text']:
-            return None
-    return py_post
 
-def get_old_posts(path):
-    if not os.path.exists(path):
-        return None
-    with open(path, mode='r', encoding='utf-8') as my_file:
+def load_old_posts(path):
+    with open(path, mode='r') as my_file:
         data = json.load(my_file)
         return data
 
+
+def is_post_unique(python_post, old_python_posts):
+    for old_python_post in old_python_posts:
+        if (python_post['text'] == old_python_post['text'])\
+                and (python_post is not old_python_post):
+            return False
+    return True
+
+
 if __name__ == "__main__":
-    #сценарий если файл уже есть
-    path = input('Enter path to DataBase:')
-    old_py_posts = get_old_posts(path)
-    if not old_py_posts:
-        print('File not found, sorry...')
-        raise SystemExit
-    py_posts = get_new_py_posts()
-    for py_post in py_posts:
-        if chek_post(py_post, old_py_posts):
+    path = input('Enter path to database:')
+    if os.path.exists(path):
+        old_py_posts = load_old_posts(path)
+    else:
+        old_py_posts = []
+    python_posts = fetch_python_posts_from_vk()
+    for py_post in python_posts:
+        if is_post_unique(py_post, python_posts):
             old_py_posts.append(py_post)
-    path = 'vk_py_groups.json'
-    with open(path, mode="w", encoding='utf-8') as my_file:
-        json.dump(py_posts, my_file)
+    with open(path, mode="w") as my_file:
+        json.dump(old_py_posts, my_file)
 
-    #сценарий если файла еще нет
-    py_posts = get_new_py_posts()
-    posts_to_add = []
-    #====================== вот это в функцию
-    for py_post in py_posts:
-        it_is_unic = True
-        for another_py_post in py_posts:
-            if ((py_post['text'] == another_py_post['text']) and (py_post['text'] is not another_py_post['text'])):
-                it_is_unic = False
-        if it_is_unic:
-            posts_to_add.append(py_post)
-    #=====================
-
-    #dump
-    path = 'vk_py_groups.json'
-    with open(path, mode="w", encoding='utf-8') as my_file:
-        json.dump(posts_to_add, my_file)
